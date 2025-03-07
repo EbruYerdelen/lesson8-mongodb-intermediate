4a8129bc63ea5ce2c76d1d287a94ee7949baa223
Ebru <yerdebru@gmail.com>
2025-03-07 15:52:56 +0300

    db update

--- database/db.js
+++ database/db.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Database connected successfully");
+      if (!process.env.MONGODB_URI) {
+        console.log("provide db url");
+        return;
+      }
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed");
     process.exit(1);
